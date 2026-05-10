586e122d45433e2131ccf74b06a8aae4c47a65a6
bistutzyy <173236231@qq.com>
2026-05-10 20:31:49 +0800

    Remove English posts and disable i18n

--- scripts/badge.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-// Reimu tag：官方 npm 包 1.12.4 未打包此脚本，供 Vercel 等干净安装使用。
-hexo.extend.tag.register('badge', function(args) {
-  let [text, color = 'blue'] = args
-  return `<mark class="rm-badge ${color}">${text}</mark>`
-})
--- scripts/mermaid.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-// Reimu tag：官方 npm 包 1.12.4 未打包此脚本，供 Vercel 等干净安装使用。
-hexo.extend.tag.register('mermaid', function(args, content) {
-  return `<div class="mermaid">${content}</div>`
-}, { ends: true })
